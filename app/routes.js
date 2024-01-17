8f5702659d4ee91cea66196b8fbc24ed139ad21b
Alexandra Aggelidis <alexandra.aggelidis@engineering.digital.dwp.gov.uk>
2024-01-17 12:02:54 +0000

    wt-3995-01
    - updated index.html to Version 3.0, archived Version 2.2
    - moved 'Pensions', 'DfC content changes', 'Green button - Signposting' and 'Backdating' to archive tab

--- app/routes.js
+++ app/routes.js
@@ -807,6 +807,91 @@ router.use(
 );
 ///////////////////////////////////////////////////////////////////////////////
 //                                                                           //
+//  ROUTING FOR VERSION 3.0                                                 //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+// This moves eligibility routing to eligibility directory
+router.use(
+  "/v3_0-citizen/1-eligibility/",
+  require("./views/v3_0-citizen/1-eligibility/_routes")
+);
+
+// This moves `abroad` routing to `abroad` directory
+router.use(
+  "/v3_0-citizen/2-claim-start/",
+  require("./views/v3_0-citizen/2-claim-start/_routes")
+);
+
+// This moves details routing to details directory
+router.use(
+  "/v3_0-citizen/3-details/",
+  require("./views/v3_0-citizen/3-details/_routes")
+);
+
+// Redirect to first question in details sequence
+router.get("/v3_0-citizen/3-details/", function (req, res) {
+  res.redirect("/v3_0-citizen/3-details/nino");
+});
+
+// This moves `other-benefits` routing to `other-benefits` directory
+router.use(
+  "/v3_0-citizen/4-other-benefits/",
+  require("./views/v3_0-citizen/4-other-benefits/_routes")
+);
+
+// This moves jury-service routing to jury-service directory
+router.use(
+  "/v3_0-citizen/5-jury-service/",
+  require("./views/v3_0-citizen/5-jury-service/_routes")
+);
+
+// This moves current-employment routing to current-employment directory
+router.use(
+  "/v3_0-citizen/6-current-employment/",
+  require("./views/v3_0-citizen/6-current-employment/_routes")
+);
+
+// This moves previous-employment routing to previous-employment directory
+router.use(
+  "/v3_0-citizen/7-previous-employment/",
+  require("./views/v3_0-citizen/7-previous-employment/_routes")
+);
+
+// This moves `abroad` routing to `abroad` directory
+router.use(
+  "/v3_0-citizen/8-abroad/",
+  require("./views/v3_0-citizen/8-abroad/_routes")
+);
+
+// This moves `pensions` routing to `pensions` directory
+router.use(
+  "/v3_0-citizen/9-pensions/",
+  require("./views/v3_0-citizen/9-pensions/_routes")
+);
+
+// This moves `education` routing to `education` directory
+router.use(
+  "/v3_0-citizen/10-education/",
+  require("./views/v3_0-citizen/10-education/_routes")
+);
+
+// This moves `availability` routing to `availability` directory
+router.use(
+  "/v3_0-citizen/11-availability/",
+  require("./views/v3_0-citizen/11-availability/_routes")
+);
+
+// This moves `error` routing to `error` directory
+router.use(
+  "/v3_0-citizen/error/",
+  require("./views/v3_0-citizen/error/_routes")
+);
+
+// {END OF ROUTING FOR VERSION 3.0} ///////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
 //  ROUTING FOR VERSION 2.2                                                 //
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
